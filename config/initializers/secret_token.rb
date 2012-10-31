f3e4739724811ebcc0722a521f016e344db96fc3
bruce <bruce@ubuntu.(none)>
2012-10-31 10:42:37 +0000

    first empty commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Barrating::Application.config.secret_token = '6bea4289d0ce118d3db38bba47aba7212483531c9b9c707be7836c0ac9e6c11922eb02bca064c7de1e1ce90046b7154120f69cfbac6f60ab6a8f5905c16cd04c'
