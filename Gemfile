07c34988cda42b10c5145feafdf5a4463bb9b525
bruce <bruce@ubuntu.(none)>
2012-11-10 21:58:30 +0000

    twitter gem added, pulls twitter feed of food and healthy

--- Gemfile
+++ Gemfile
@@ -19,6 +19,8 @@ end
 
 gem 'devise'
 
+gem 'twitter'
+
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+  Twitter.configure do |config|
+      config.consumer_key = 'SGWdRHMhxxOb4uOxNa6UQ'
+      config.consumer_secret = 'diyz0MTK36pr4shdGKAzueFVXTyAYiyhbWTRVkkwRI'
+      config.oauth_token = '464745491-iD8SPXjiMANV6buEuqvvtNUZ8lagUXHvzugEPN44'
+      config.oauth_token_secret = 'ni4VHRXqcJ6h9EQvZlb5GsmsyL6AUXzFpZgdHLgg'
+  end
