875b32b5f4f1cd81e3e1c460d02288c98ca91ad5
Mark <mark@mark-virtual-machine.(none)>
2012-11-13 11:28:04 +0000

    paperclip stuff added to production.rb to see if it fixes heroku error

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ActiveRecord::Base
   :styles => { :medium => "300x300>", :thumb => "100x100>" },
     :storage => :s3,
     :url => ":s3_domain_url",
-    :path => "/:class/photos/:id_:basename.:style.:extension",
+    :path => "/:class/avatars/:id_:basename.:style.:extension",
     :s3_credentials => {
       :bucket            => ENV['S3_BUCKET_NAME'],
       :access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
