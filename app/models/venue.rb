4745b43449ab5755bbcc620587c6cd4883d77581
bruce <bruce@ubuntu.(none)>
2012-11-13 12:34:20 +0000

    heroku error fix

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,15 +1,10 @@
 class Venue < ActiveRecord::Base
   attr_accessible :photo, :city, :address, :health_rating, :health_rating_date, :latitude, :longitude, :name, :postcode, :review, :avg_star_rating, :street, :business_id, :address1, :address2, :address3, :address4
 	has_many :reviews, order: 'updated_at desc'
-<<<<<<< HEAD
   
    validates :business_id, :uniqueness => true
    
-=======
 
-   #validates :business_id, :uniqueness => true
-
->>>>>>> e681238d8bf7c8a53cf4c8c08c113e8851358689
 #    has_many :reviewers, :through => :reviews, :source => :users
 
   if Rails.env.production?
